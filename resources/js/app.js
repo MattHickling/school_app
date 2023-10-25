0b3d7d2886cdf42ab8921f33c809b4c9abd3d9d5
Matt Hickling <m.hickling@hotmail.co.uk>
2023-10-25 17:56:06 +0100

    breeze  installed with alpine

--- resources/js/app.js
+++
@@ -1,3 +0,0 @@
-import './bootstrap';
-import toastr from 'toastr';
-window.toastr = toastr;
--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ClassesController;
-use App\Http\Controllers\ClassroomController;
-use App\Http\Controllers\TeacherController;
-use App\Http\Controllers\TeachingAssistantController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
-    return view('home');
-})->name('home');
+    return ['Laravel' => app()->version()];
+});
 
-Route::get('/main/create', [HomeController::class, 'create'])->name('main.create');
-Route::post('/main', [HomeController::class, 'store'])->name('main.store');
-
-// Classroom routes
-Route::get('/classrooms/create', [ClassroomController::class, 'create'])->name('classrooms.create');
-Route::post('/classrooms', [ClassroomController::class, 'store'])->name('classrooms.store');
-
-// Teacher routes
-Route::get('/teachers/create', [TeacherController::class, 'create'])->name('teachers.create');
-Route::post('/teachers', [TeacherController::class, 'store'])->name('teachers.store');
-
-// Teaching Assistant routes
-Route::get('/teaching-assistants/create', [TeachingAssistantController::class, 'create'])->name('teaching-assistants.create');
-Route::post('/teaching-assistants', [TeachingAssistantController::class, 'store'])->name('teaching-assistants.store');
-Route::get('/teachers', [TeacherController::class, 'index'])->name('teachers.index');
-
-Route::get('/school-selector', [ClassesController::class, 'displayForm'])->name('school-selector');
-Route::post('/process-selection', [ClassesController::class, 'processSelection'])->name('process.selection');
-
-Route::get('/main/create', [HomeController::class, 'main.create']);
-Route::post('/process-selection', [HomeController::class, 'processSelection'])->name('process.selection');
-
-Route::get('/main/create', [HomeController::class, 'create'])->name('main.create');
-
-Route::post('/select-teachers', [ClassesController::class, 'selectTeachers'])->name('select.teachers');
+require __DIR__.'/auth.php';
