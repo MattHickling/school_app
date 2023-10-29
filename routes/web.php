4bb2798e863ca2450256b7f6bc5ff925cff930da
Matt Hickling <m.hickling@hotmail.co.uk>
2023-10-29 10:08:46 +0000

    Routes fixed

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
 
-Route::post('/login', 'Auth\LoginController@login');
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+    return view('home');
+})->name('home');
 
-require __DIR__.'/auth.php';
+Route::get('/main/create', [HomeController::class, 'create'])->name('main.create');
+Route::post('/main', [HomeController::class, 'store'])->name('main.store');
+
+// Classroom routes
+Route::get('/classrooms/create', [ClassroomController::class, 'create'])->name('classrooms.create');
+Route::post('/classrooms', [ClassroomController::class, 'store'])->name('classrooms.store');
+
+// Teacher routes
+Route::get('/teachers/create', [TeacherController::class, 'create'])->name('teachers.create');
+Route::post('/teachers', [TeacherController::class, 'store'])->name('teachers.store');
+
+// Teaching Assistant routes
+Route::get('/teaching-assistants/create', [TeachingAssistantController::class, 'create'])->name('teaching-assistants.create');
+Route::post('/teaching-assistants', [TeachingAssistantController::class, 'store'])->name('teaching-assistants.store');
+Route::get('/teachers', [TeacherController::class, 'index'])->name('teachers.index');
+
+Route::get('/school-selector', [ClassesController::class, 'displayForm'])->name('school-selector');
+Route::post('/process-selection', [ClassesController::class, 'processSelection'])->name('process.selection');
+
+Route::get('/main/create', [HomeController::class, 'main.create']);
+Route::post('/process-selection', [HomeController::class, 'processSelection'])->name('process.selection');
+
+Route::get('/main/create', [HomeController::class, 'create'])->name('main.create');
+
+Route::post('/select-teachers', [ClassesController::class, 'selectTeachers'])->name('select.teachers');
