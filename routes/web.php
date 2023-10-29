b3ae018e82367c9eb75e3261567cd24002125e02
Matt Hickling <m.hickling@hotmail.co.uk>
2023-10-29 13:01:30 +0000

    removed updated dependencies for breeze

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
 Route::get('/', function () {
     return view('home');
 })->name('home');
@@ -43,4 +41,3 @@ Route::post('/process-selection', [HomeController::class, 'processSelection'])->
 
 Route::get('/main/create', [HomeController::class, 'create'])->name('main.create');
 
-Route::post('/select-teachers', [ClassesController::class, 'selectTeachers'])->name('select.teachers');
