21f85e691ba319689b9e4cc9ee4d7163c0b79935
Matt Hickling <m.hickling@hotmail.co.uk>
2023-10-30 18:01:10 +0000

    routes fixed

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\ClassroomController;
 
 /*
 |--------------------------------------------------------------------------
