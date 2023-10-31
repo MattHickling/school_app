12dd3680daea3a49ea8f5bf9e5fe41bbd99d2efe
Matt Hickling <m.hickling@hotmail.co.uk>
2023-10-31 19:26:24 +0000

    class update

--- app/Http/Controllers/ClassroomController.php
+++ app/Http/Controllers/ClassroomController.php
@@ -1,63 +1,29 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Classroom;
 use App\Models\Teacher;
-use App\Models\TeachingAssistant;
-
-use Illuminate\View\View;
 use Illuminate\Http\Request;
-
 
 class ClassroomController extends Controller
 {
-    public function index()
-{
-    $classrooms = Classroom::all(); 
+    public function create()
+    {
+        $teachers = Teacher::all();
+        return view('classrooms.create', compact('teachers'));
+    }
 
-   
-    return view('classrooms.index', compact('classroom'));}
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'age_of_children' => 'required',
+            'number_of_pupils' => 'required',
+        ]);
 
-public function show($id)
-{
-    // Logic for displaying a specific resource
-}
+        Classroom::create($data);
 
-public function create()
-{
-    $teachers = Teacher::all();
-    return view('classrooms.create', compact('teachers'));
-}
-
-
-public function store(Request $request)
-{
-    $data = $request->validate([
-        'name' => 'nullable', // Make 'name' field optional
-        'age_of_children' => 'required',
-        'number_of_pupils' => 'required',
-    ]);
-    $teachers = Teacher::all();
-
-
-    Classroom::create($data);
-    
-    return redirect()->route('classrooms.create')->with('toastr_message', 'Classroom created successfully');
-}
-
-
-public function edit($id)
-{
-    // Logic for displaying the edit form
-}
-
-public function update(Request $request, $id)
-{
-    // Logic for updating an existing resource
-}
-
-public function destroy($id)
-{
-    // Logic for deleting a resource
-}
+        return redirect()->route('classrooms.create')->with('success', 'Classroom created successfully');
+    }
 }
