d973f7a02a6955b7bfc54ee643308a0e244829c5
Matt Hickling <m.hickling@hotmail.co.uk>
2023-12-02 10:58:39 +0000

    home page style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,27 +3,28 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 offset-md-2 text-center">
+        <div class="col-md-8">
             <h1>Welcome to Your School Management App</h1>
             <p>Manage Teachers, Teaching Assistants, Classrooms, and More</p>
-
-            <div class="row">
-                <div class="col-md-6">
-                    <a href="{{ route('teachers.create') }}" class="btn btn-info btn-block btn-lg">Add Teacher</a>
-                </div>
-                <div class="col-md-6">
-                    <a href="{{ route('teaching-assistants.create') }}" class="btn btn-warning btn-block btn-lg">Add Teaching Assistant</a>
-                </div>
-                <div class="col-md-6 mt-2">
-                    <a href="{{ route('classrooms.create') }}" class="btn btn-success btn-block btn-lg">Add Classroom</a>
-                </div>
-                <div class="col-md-6 mt-2">
-                    <a href="{{ route('school_years.create') }}" class="btn btn-primary btn-block btn-lg">Add School</a>
-                </div>
-                <div class="col-md-12 mt-2"> 
-                    <a class="nav-link btn btn-secondary btn-block" href="{{ route('school_years.show_classes') }}">Show School Year Classes</a>
-                </div>
-            </div>
+        </div>
+        <div class="col-md-4">
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <a href="{{ route('teachers.create') }}" class="btn btn-outline-dark btn-block">Add Teacher</a>
+                </li>
+                <li class="list-group-item">
+                    <a href="{{ route('teaching-assistants.create') }}" class="btn btn-outline-dark btn-block">Add Teaching Assistant</a>
+                </li>
+                <li class="list-group-item">
+                    <a href="{{ route('classrooms.create') }}" class="btn btn-outline-dark btn-block">Add Classroom</a>
+                </li>
+                <li class="list-group-item">
+                    <a href="{{ route('school_years.create') }}" class="btn btn-outline-dark btn-block">Add School</a>
+                </li>
+                <li class="list-group-item">
+                    <a href="{{ route('school_years.show_classes') }}" class="btn btn-outline-dark btn-block">Show School Year Classes</a>
+                </li>
+            </ul>
         </div>
     </div>
 </div>
