95a7b5e12652a3379631baf815f56bed7e31fc74
Matt Hickling <m.hickling@hotmail.co.uk>
2023-12-08 19:13:00 +0000

    re-style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,35 @@
 @extends('layouts.footer')
 
 @section('content')
-<div class="container">
+<div class="container mt-5">
     <div class="row">
-        <div class="col-md-8">
-            <h1>Welcome to Your School Management App</h1>
-            <p>Manage Teachers, Teaching Assistants, Classrooms, and More</p>
-        </div>
         <div class="col-md-4">
-            <ul class="list-group">
-                <li class="list-group-item">
-                    <a href="{{ route('teachers.create') }}" class="btn btn-outline-dark btn-block">Add Teacher</a>
-                </li>
-                <li class="list-group-item">
-                    <a href="{{ route('teaching-assistants.create') }}" class="btn btn-outline-dark btn-block">Add Teaching Assistant</a>
-                </li>
-                <li class="list-group-item">
-                    <a href="{{ route('classrooms.create') }}" class="btn btn-outline-dark btn-block">Add Classroom</a>
-                </li>
-                <li class="list-group-item">
-                    <a href="{{ route('school_years.create') }}" class="btn btn-outline-dark btn-block">Add School</a>
-                </li>
-                <li class="list-group-item">
-                    <a href="{{ route('school_years.show_classes') }}" class="btn btn-outline-dark btn-block">Show School Year Classes</a>
-                </li>
-            </ul>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Quick Actions</h5>
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <a href="{{ route('teachers.create') }}" class="btn btn-outline-primary btn-block">Add Teacher</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="{{ route('teaching-assistants.create') }}" class="btn btn-outline-primary btn-block">Add Teaching Assistant</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="{{ route('classrooms.create') }}" class="btn btn-outline-primary btn-block">Add Classroom</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="{{ route('school_years.create') }}" class="btn btn-outline-primary btn-block">Add School</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="{{ route('school_years.show_classes') }}" class="btn btn-outline-primary btn-block">Show School Year Classes</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <h1 class="display-4 font-weight-bold">Welcome to Your School Management App</h1>
+            <p class="lead">Effortlessly manage teachers, teaching assistants, classrooms, and more.</p>
         </div>
     </div>
 </div>
